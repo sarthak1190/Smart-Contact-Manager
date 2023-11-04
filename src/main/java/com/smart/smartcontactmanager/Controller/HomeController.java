4a4e959ba269859a4bf9b54b9dd8854eac2aab99
sarthak1190 <sarthak1190@gmail.com>
2023-11-05 00:46:41 +0530

    changes in files

--- src/main/java/com/smart/smartcontactmanager/Controller/HomeController.java
+++ src/main/java/com/smart/smartcontactmanager/Controller/HomeController.java
@@ -3,6 +3,7 @@ package com.smart.smartcontactmanager.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.smart.smartcontactmanager.Entities.User;
 import com.smart.smartcontactmanager.Helper.Message;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -46,12 +48,18 @@ public class HomeController {
     }
 
     @PostMapping("/do_register")
-    public String registerUser(@ModelAttribute("user") User user, @RequestParam(value = "agreement",defaultValue = "false") boolean agreement, Model m, HttpSession session){
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, @RequestParam(value = "agreement",defaultValue = "false") boolean agreement, Model m, HttpSession session){
         
         try {
             if(!agreement){
             System.out.println("You have not agreed the terms and conditions");
             throw new Exception("You have not agreed the terms and conditions");
+        }
+
+        if(result.hasErrors()){
+            System.out.println("Error "+result.toString());
+            m.addAttribute("user", user);
+            return "signup";
         }
         user.setRole("ROLE_USER");
         user.setEnabled(true);
@@ -59,7 +67,7 @@ public class HomeController {
 
         System.out.println("Agreement: "+agreement);
         System.out.println("USER: "+user);
-        User result=userRepository.save(user);
+        userRepository.save(user);
         m.addAttribute("user", new User());
         m.addAttribute("message", new Message("Successfully Registered !!","alert-success"));
         return "signup";
--- src/main/java/com/smart/smartcontactmanager/Entities/User.java
+++ src/main/java/com/smart/smartcontactmanager/Entities/User.java
@@ -12,6 +12,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="USER")
@@ -20,8 +26,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @NotBlank(message = "Name field can't be empty !!")
+    @Size(min = 2, max = 20, message = "min 2 and max 20 characters are allowed")
     private String name;
     @Column(unique = true)
+    @Email
     private String email;
     private String password;
     private String role;
